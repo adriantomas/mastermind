07eebbf7d4b57c8d7ea19430625a64658340677d
Adrián <dev@adriantomas.com>
2020-11-10 12:56:45 -0100

    feat: add game creation lambda

---
+++ api/game/lambdas/create_game.py
@@ -0,0 +1,43 @@
+"""
+"""
+import os
+from datetime import datetime
+from json import dumps
+from random import randrange
+from uuid import uuid4
+
+import boto3
+from aws_lambda_powertools import Logger, Tracer
+
+TABLE_NAME = os.getenv("DYNAMODB_TABLE_NAME")
+MAX_TRIES = 8
+
+tracer = Tracer()
+logger = Logger()
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(TABLE_NAME)
+
+
+@tracer.capture_lambda_handler
+@logger.inject_lambda_context
+def handler(event, context) -> dict:
+    return process_event(event)
+
+
+def process_event(event: dict) -> dict:
+    secret = randrange(1000, 10000)
+    game_id = str(uuid4())
+    _save_game(game_id, secret)
+
+    return {"statusCode": 200, "body": dumps({"game_id": game_id})}
+
+
+def _save_game(game_id: str, secret: str) -> None:
+    table.put_item(
+        Item={
+            "pk": f"GAM#{game_id}",
+            "secret": secret,
+            "tries_left": MAX_TRIES,
+            "created_at": datetime.utcnow().isoformat(),
+        }
+    )
